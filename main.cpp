e31ff6f66e83ec86666ecc26bdcec242a53fabf0
Onibaku789 <varga.attila798@gmail.com>
2017-10-25 17:23:45 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+/* 
+ * File:   main.cpp
+ * Author: thedankmemeguy
+ *
+ * Created on 19 October 2017, 14:34
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
